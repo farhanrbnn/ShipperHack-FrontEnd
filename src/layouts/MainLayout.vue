3131954b04de5b6b4012066b925f8685c447af10
farhan <farhan.rabbaanii@binus.ac.id>
2021-04-10 15:58:55 +0700

    add landing page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,15 +1,59 @@
 <!-- HEADER APP -->
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-
-        <q-toolbar-title>
-          Noob But Who Cares ?
-        </q-toolbar-title>
-
-      </q-toolbar>
+    <q-header class="top-header">
     </q-header>
+     <q-drawer 
+        show-if-above
+        :width="250"
+        :breakpoint="500"
+        bordered
+        content-class="bg-grey-3"
+      >
+        <q-scroll-area class="fit">
+          <q-list>
+
+            <template>
+              <div class="flex flex-center">
+                <img src="../assets/Logo.png">
+                <h5 class="flex flex-center">Warehouse NBWC</h5>
+              </div>
+              <q-expansion-item
+                icon="perm_identity"
+                label="Put List">
+                <q-card class="bg-grey-9">
+                 <!--  <q-card-section>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                    commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                    eveniet doloribus ullam aliquid.
+                  </q-card-section> -->
+                </q-card>
+              </q-expansion-item>
+              <q-expansion-item
+                icon="perm_identity"
+                label="Report">
+                <q-card class="bg-grey-9">
+                 <!--  <q-card-section>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                    commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                    eveniet doloribus ullam aliquid.
+                  </q-card-section> -->
+                </q-card>
+              </q-expansion-item>
+              <!-- <q-item :key="index" clickable :active="menuItem.label === 'Outbox'" v-ripple>
+                <q-item-section avatar>
+                  <q-icon :name="menuItem.icon" />
+                </q-item-section>
+                <q-item-section>
+                  {{ menuItem.label }}
+                </q-item-section>
+              </q-item> -->
+              <!-- <q-separator :key="'sep' + index"  v-if="menuItem.separator" /> -->
+            </template>
+
+          </q-list>
+        </q-scroll-area>
+      </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -18,58 +62,31 @@
 </template>
 
 <script>
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
 export default {
-  name: 'MainLayout',
-  data() {
-    return {
+  name: 'MainLayout'
+  // data() {
+  //   return {
+  //     drawer: false,
+  //     menuList
 
-    }
-  }
+  //   }
+  // }
 }
 </script>
+<style lang="scss">
+img {
+  margin-top: 13px;
+}
+
+.top-header {
+  height: 80px;
+  background-color: #FAFAFA;
+}
+
+.fit {
+  background-color: #404040;
+  color: white;
+  font-size: 18px;
+}
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,38 @@
 <template>
-  <q-page class="flex flex-center">
-  	<router-link to="/next">
-	   <!--  <img
-	      alt="Quasar logo"
-	      src="~assets/quasar-logo-full.svg"
-	    > -->
-	    <h1>HOME PAGE</h1>
-    </router-link>
+  <q-page>
+  	<div id="main-row" class="row">
+     <div class="col-12">
+       <p class="title">Active Jobs</p>
+       <q-separator color="#EFEFEF" inset />
+
+       <div class="active">
+        <q-card flat bordered class="active">
+          <q-card-section class="q-pt-none">
+            <div class="row">
+              <div class="col-3">
+                <p>Date</p>
+                <h4>10/04/21</h4>
+              </div>
+              <div class="col-3">
+                <p>P.O.ID</p>
+                <h4>IB10002</h4>
+              </div>
+            </div>
+          </q-card-section>
+         </q-card>
+         <q-table
+          :data="data"
+          :columns="columns"
+          row-key="name"
+        />
+        </div>
+       <!-- <div class="no-active">
+         <p>No Active Data</p>
+         <q-btn unelevated color="primary" label="Generate Pick List From Inbound" />
+       </div> -->
+     </div> 
+     <hr>
+    </div>
   </q-page>
 </template>
 
@@ -15,6 +41,24 @@ import DataService from '../boot/axios'
 
 export default {
   name: 'PageIndex',
+  data () {
+    return {
+      columns: [
+        {
+          name: 'name',
+          required: true,
+          align: 'left'
+        },
+        { name: 'calories', align: 'center', label: 'Category', field: 'calories', sortable: true },
+        { name: 'fat', align: 'center', label: 'Goods Qty.', field: 'fat', sortable: true },
+        { name: 'carbs', align: 'center', label: 'Exp. Date', field: 'carbs' },
+        { name: 'protein', align: 'center',label: 'Category', field: 'protein' },
+        { name: 'sodium', align: 'center', label: 'Location Bin', field: 'sodium' },
+        { name: 'calcium', align: 'center', label: 'Status', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+        { name: 'iron', align: 'center', label: 'Action', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+      ]
+    }
+  },
   created() {
   	DataService.get()
   	.then((res)=>{
@@ -26,3 +70,36 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+h4 {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.q-pt-none {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.no-active {
+  font-size: 18px;
+  margin-top: 20px;
+}
+
+.title {
+  font-size: 24px;
+  font-weight: bold; 
+}
+
+#main-row {
+  padding-top: 50px;
+}
+
+.col-12 {
+  padding-left: 80px;
+  padding-right: 80px;
+}  
+
+.active {
+  margin-top: 20px;
+  background-color: #EFEFEF;
+}
+</style>
