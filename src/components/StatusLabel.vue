9b8bbaca18b5cda2ac236bd828439c91488d9b01
Kahlil Iman <kahlil.iman@gmail.com>
2021-04-10 19:19:11 +0700

    add StatusLabel component

---
+++ src/components/StatusLabel.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-if="labelType === 'ai-assigned'" class="status-label label-green">
+      <p class="status-label__text"><q-icon name="fa fa-magic" />Assigned</p>
+    </div>
+
+    <div
+      v-if="labelType === 'manual-assigned'"
+      class="status-label label-green"
+    >
+      <p class="status-label__text"><q-icon name="fa fa-check" />Assigned</p>
+    </div>
+
+    <div v-if="labelType === 'stored'" class="status-label label-blue">
+      <p class="status-label__text">Stored</p>
+    </div>
+
+    <div v-if="labelType === 'pending'" class="status-label label-grey">
+      <p class="status-label__text">Pending</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StatusLabel",
+  props: {
+    labelType: String
+  }
+};
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,77 +1,108 @@
 <template>
   <q-page>
-  	<div id="main-row" class="row">
-     <div class="col-12">
-       <p class="title">Active Jobs</p>
-       <q-separator color="#EFEFEF" inset />
-
-       <div class="active">
-        <q-card flat bordered class="active">
-          <q-card-section class="q-pt-none">
-            <div class="row">
-              <div class="col-3">
-                <p>Date</p>
-                <h4>10/04/21</h4>
+    <div id="main-row" class="row">
+      <div class="col-12">
+        <p class="title">Active Jobs</p>
+        <q-separator color="#EFEFEF" inset />
+        <div class="active">
+          <q-card flat bordered class="active">
+            <q-card-section class="q-pt-none">
+              <div class="row">
+                <div class="col-3">
+                  <p>Date</p>
+                  <h4>10/04/21</h4>
+                </div>
+                <div class="col-3">
+                  <p>P.O.ID</p>
+                  <h4>IB10002</h4>
+                </div>
               </div>
-              <div class="col-3">
-                <p>P.O.ID</p>
-                <h4>IB10002</h4>
-              </div>
-            </div>
-          </q-card-section>
-         </q-card>
-         <q-table
-          :data="data"
-          :columns="columns"
-          row-key="name"
-        />
+            </q-card-section>
+          </q-card>
+          <q-table :data="data" :columns="columns" row-key="name" />
         </div>
-       <!-- <div class="no-active">
+        <!-- <div class="no-active">
          <p>No Active Data</p>
          <q-btn unelevated color="primary" label="Generate Pick List From Inbound" />
        </div> -->
-     </div> 
-     <hr>
+      </div>
+      <hr />
     </div>
   </q-page>
 </template>
 
 <script>
-import DataService from '../boot/axios'
+import DataService from "../boot/axios";
 
 export default {
-  name: 'PageIndex',
-  data () {
+  name: "PageIndex",
+  components: {},
+  data() {
     return {
       columns: [
         {
-          name: 'name',
+          name: "name",
           required: true,
-          align: 'left'
+          align: "left"
         },
-        { name: 'calories', align: 'center', label: 'Category', field: 'calories', sortable: true },
-        { name: 'fat', align: 'center', label: 'Goods Qty.', field: 'fat', sortable: true },
-        { name: 'carbs', align: 'center', label: 'Exp. Date', field: 'carbs' },
-        { name: 'protein', align: 'center',label: 'Category', field: 'protein' },
-        { name: 'sodium', align: 'center', label: 'Location Bin', field: 'sodium' },
-        { name: 'calcium', align: 'center', label: 'Status', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
-        { name: 'iron', align: 'center', label: 'Action', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+        {
+          name: "calories",
+          align: "center",
+          label: "Category",
+          field: "calories",
+          sortable: true
+        },
+        {
+          name: "fat",
+          align: "center",
+          label: "Goods Qty.",
+          field: "fat",
+          sortable: true
+        },
+        { name: "carbs", align: "center", label: "Exp. Date", field: "carbs" },
+        {
+          name: "protein",
+          align: "center",
+          label: "Category",
+          field: "protein"
+        },
+        {
+          name: "sodium",
+          align: "center",
+          label: "Location Bin",
+          field: "sodium"
+        },
+        {
+          name: "calcium",
+          align: "center",
+          label: "Status",
+          field: "calcium",
+          sortable: true,
+          sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
+        },
+        {
+          name: "iron",
+          align: "center",
+          label: "Action",
+          field: "iron",
+          sortable: true,
+          sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
+        }
       ]
-    }
+    };
   },
   created() {
-  	DataService.get()
-  	.then((res)=>{
-  		console.log(res)
-  	})
-  	.catch((err)=>{
-  		console.log(err)
-  	})
+    DataService.get()
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
-}
+};
 </script>
 <style lang="scss">
-
 h4 {
   margin-top: 10px;
   margin-bottom: 10px;
@@ -87,7 +118,7 @@ h4 {
 
 .title {
   font-size: 24px;
-  font-weight: bold; 
+  font-weight: bold;
 }
 
 #main-row {
@@ -97,10 +128,10 @@ h4 {
 .col-12 {
   padding-left: 80px;
   padding-right: 80px;
-}  
+}
 
 .active {
   margin-top: 20px;
-  background-color: #EFEFEF;
+  background-color: #efefef;
 }
 </style>
