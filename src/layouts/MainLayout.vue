46dc21876dddcd6268bc69cc51f465d322bd979c
Kahlil Iman <kahlil.iman@gmail.com>
2021-04-10 19:56:26 +0700

    change variables & icons

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,46 +1,40 @@
 <!-- HEADER APP -->
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header class="top-header">
-    </q-header>
-     <q-drawer 
-        show-if-above
-        :width="250"
-        :breakpoint="500"
-        bordered
-        content-class="bg-grey-3"
-      >
-        <q-scroll-area class="fit">
-          <q-list>
-
-            <template>
-              <div class="flex flex-center">
-                <img src="../assets/Logo.png">
-                <h5 class="flex flex-center">Warehouse NBWC</h5>
-              </div>
-              <q-expansion-item
-                icon="perm_identity"
-                label="Put List">
-                <q-card class="bg-grey-9">
-                 <!--  <q-card-section>
+    <q-header class="top-header"> </q-header>
+    <q-drawer
+      show-if-above
+      :width="250"
+      :breakpoint="500"
+      bordered
+      content-class="bg-grey-3"
+    >
+      <q-scroll-area class="fit">
+        <q-list>
+          <template>
+            <div class="flex flex-center">
+              <img src="../assets/Logo.png" />
+              <h5 class="flex flex-center">Warehouse NBWC</h5>
+            </div>
+            <q-expansion-item icon="fa fa-box-open" label="Put List">
+              <q-card class="bg-grey-9">
+                <!--  <q-card-section>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
                     commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
                     eveniet doloribus ullam aliquid.
                   </q-card-section> -->
-                </q-card>
-              </q-expansion-item>
-              <q-expansion-item
-                icon="perm_identity"
-                label="Report">
-                <q-card class="bg-grey-9">
-                 <!--  <q-card-section>
+              </q-card>
+            </q-expansion-item>
+            <q-expansion-item icon="fa fa-chart-bar" label="Report">
+              <q-card class="bg-grey-9">
+                <!--  <q-card-section>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
                     commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
                     eveniet doloribus ullam aliquid.
                   </q-card-section> -->
-                </q-card>
-              </q-expansion-item>
-              <!-- <q-item :key="index" clickable :active="menuItem.label === 'Outbox'" v-ripple>
+              </q-card>
+            </q-expansion-item>
+            <!-- <q-item :key="index" clickable :active="menuItem.label === 'Outbox'" v-ripple>
                 <q-item-section avatar>
                   <q-icon :name="menuItem.icon" />
                 </q-item-section>
@@ -48,12 +42,11 @@
                   {{ menuItem.label }}
                 </q-item-section>
               </q-item> -->
-              <!-- <q-separator :key="'sep' + index"  v-if="menuItem.separator" /> -->
-            </template>
-
-          </q-list>
-        </q-scroll-area>
-      </q-drawer>
+            <!-- <q-separator :key="'sep' + index"  v-if="menuItem.separator" /> -->
+          </template>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -63,7 +56,7 @@
 
 <script>
 export default {
-  name: 'MainLayout'
+  name: "MainLayout"
   // data() {
   //   return {
   //     drawer: false,
@@ -71,7 +64,7 @@ export default {
 
   //   }
   // }
-}
+};
 </script>
 <style lang="scss">
 img {
@@ -80,7 +73,7 @@ img {
 
 .top-header {
   height: 80px;
-  background-color: #FAFAFA;
+  background-color: #fafafa;
 }
 
 .fit {
@@ -88,5 +81,4 @@ img {
   color: white;
   font-size: 18px;
 }
-
 </style>
